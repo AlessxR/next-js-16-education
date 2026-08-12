97179a7fbf5b49dbb34880c2c1c9fe69afcb37a6
AlessxR <alexxsrs.al@gmail.com>
2026-08-12 22:02:45 +0300

    feat: add more page.tsx and layout.tsx for specific url

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 // next.js waiting for a export default function or a component
 export default function DashboardPage() {
-    return <div>Dashboard</div>;
+    return <div className="p-4 bg-emerald-50 border-2 border-emerald-500 rounded-md">
+        <h1 className='text-xl font-bold text-black'>Content via Dashboard</h1>
+    </div>;
 }
 
---
+++ src/app/dashboard/settings/page.tsx
@@ -0,0 +1,8 @@
+// next.js waiting for a export default function or a component
+export default function SettingsPage() {
+    return <div className="p-4 bg-zinc-50 border-2 border-zinc-500 rounded-md">
+        <h1 className='text-xl font-bold text-black'>Content via Settings</h1>
+        <p className='mt-2 text-zinc-600'>This page is located in /dashboard/settings</p>
+    </div>;
+}
+
