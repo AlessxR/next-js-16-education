168506ae6b3798db5a88a44377b77e446803ddd2
AlessxR <alexxsrs.al@gmail.com>
2026-08-13 14:48:19 +0300

    feat: add loading.tsx, display loading while data is loading

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
 // next.js waiting for a export default function or a component
-export default function DashboardPage() {
-    return <div className="p-4 bg-emerald-50 border-2 border-emerald-500 rounded-md">
-        <h1 className='text-xl font-bold text-black'>Content via Dashboard</h1>
-    </div>;
-}
+export default async function DashboardPage() {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
 
+    return (
+        <div className="p-4 bg-emerald-50 border-2 border-emerald-500 rounded-md">
+            <h1 className="text-xl font-bold text-black">
+                Content via Dashboard
+            </h1>
+        </div>
+    );
+}
